ab575394c95734be7733b17c8a9213fe8f95c75c
wlc <P775_Vmware@Ubuntu24_04_20250722>
2026-04-20 14:32:01 +0800

    example from the book; /dev/eep-mem /sys/class/eep-class/eep-mem

---
+++ Linux_Kernel_Learning/04_Example/016_register_char_device/register_char_device.c
@@ -0,0 +1,98 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+#define EEP_NBANK 8
+#define EEP_DEVICE_NAME "eep-mem"
+#define EEP_CLASS "eep-class"
+
+struct class *eep_class;
+struct cdev eep_cdev[EEP_NBANK];
+dev_t dev_num;
+
+
+static int eep_open(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static int eep_release(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static ssize_t eep_read(struct file *file, char __user *buf,
+                        size_t count, loff_t *ppos)
+{
+    return 0;
+}
+
+static ssize_t eep_write(struct file *file, const char __user *buf,
+                         size_t count, loff_t *ppos)
+{
+    return count;
+}
+
+static const struct file_operations eep_fops = {
+    .owner   = THIS_MODULE,
+    .open    = eep_open,
+    .release = eep_release,
+    .read    = eep_read,
+    .write   = eep_write,
+    // .llseek, .ioctl, etc. if needed
+};
+
+static int __init register_char_device_init(void)
+{
+    int i;
+    dev_t curr_dev;
+    alloc_chrdev_region(&dev_num, 0, EEP_NBANK, EEP_DEVICE_NAME);
+
+    eep_class = class_create(EEP_CLASS);
+
+    for (int i = 0; i < EEP_NBANK; ++i) {
+        cdev_init(&eep_cdev[i], &eep_fops);
+        eep_cdev[i].owner = THIS_MODULE;
+        
+        curr_dev = MKDEV(MAJOR(dev_num), MINOR(dev_num) + i);
+        cdev_add(&eep_cdev[i], curr_dev, 1);
+        device_create(eep_class, NULL, curr_dev, NULL, EEP_DEVICE_NAME "%d", i);
+    }
+
+    printk(KERN_INFO "WLC load\n");
+
+    return 0;
+}
+
+static void __exit register_char_device_exit(void)
+{
+    int i;
+    dev_t curr_dev;
+
+    for (i = 0; i < EEP_NBANK; i++) {
+        curr_dev = MKDEV(MAJOR(dev_num), MINOR(dev_num) + i);
+
+        /* Remove /dev node */
+        device_destroy(eep_class, curr_dev);
+
+        /* Remove cdev from kernel */
+        cdev_del(&eep_cdev[i]);
+    }
+
+    /* Destroy class (/sys/class/eep-class) */
+    class_destroy(eep_class);
+
+    /* Release device numbers */
+    unregister_chrdev_region(dev_num, EEP_NBANK);
+
+    printk(KERN_INFO "WLC unload\n");
+
+}
+
+module_init(register_char_device_init);
+module_exit(register_char_device_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shirley Ra");
+MODULE_DESCRIPTION("char device register");
